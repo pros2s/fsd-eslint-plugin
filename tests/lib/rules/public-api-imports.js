6de7ccbfffa97525ab3ee94c9c254ee4df21d39f
Alexandr Podoplelov <caiiiia_podoplelov@mail.ru>
2023-01-19 18:06:15 +0300

    testing files should be exported and imported from testing public api (testing.ts)

--- tests/lib/rules/public-api-imports.js
+++ tests/lib/rules/public-api-imports.js
@@ -39,6 +39,16 @@ ruleTester.run('public-api-imports', rule, {
       errors: [],
       options: aliasOptions,
     },
+    {
+      filename: 'D:\\vs\\react\\middle-project\\src\\entities\\file.test.ts',
+      code: "import { ArticleCodeBlock } from 'entities/article/testing'",
+      options: [{ ...aliasOptions, testFilesPatterns: ['**/*.test.ts'] }],
+    },
+    {
+      filename: 'D:\\vs\\react\\middle-project\\src\\entities\\file.stories.tsx',
+      code: "import { ArticleCodeBlock } from 'entities/article/testing'",
+      options: [{ ...aliasOptions, testFilesPatterns: ['**/*.stories.tsx'] }],
+    },
   ],
 
   invalid: [
@@ -46,6 +56,18 @@ ruleTester.run('public-api-imports', rule, {
       code: "import { ArticleCodeBlock } from '@/entities/article/ui/ArticleCodeBlock/ArticleCodeBlock'",
       errors: [{ message: 'Absolute paths are allowed only from public api (index.ts)' }],
       options: aliasOptions,
+    },
+    {
+      filename: 'D:\\vs\\react\\middle-project\\src\\entities\\file.stories.tsx',
+      code: "import { ArticleCodeBlock } from 'entities/article/testing/file.tsx'",
+      errors: [{ message: 'Absolute paths are allowed only from public api (index.ts)' }],
+      options: [{ ...aliasOptions, testFilesPatterns: ['**/*.stories.tsx'] }],
+    },
+    {
+      filename: 'D:\\vs\\react\\middle-project\\src\\entities\\file.stories.tsx',
+      code: "import { ArticleCodeBlock } from 'entities/article/testing'",
+      errors: [{ message: 'Testing data are allowed only from testing public api (testing.ts)' }],
+      options: [{ ...aliasOptions, testFilesPatterns: ['**/*.test.tsx'] }],
     },
   ],
 });
